d336f7cd87970ef5f9168d4ca37d3cf6c50e554b
Faizan Sheraz <faizansheraz212101020@gmail.com>
2025-06-02 18:43:21 +0500

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+buildscript {
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.9.2")
+        classpath("com.google.gms:google-services:4.4.1")
+    }
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
